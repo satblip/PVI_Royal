09bf781ce8ffafe34e8c486d71d30a514979cb55
Satblip <sat@satprod.net>
2014-02-12 09:57:30 +0100

    Backend Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Royaltervueren::Application.config.secret_key_base = 'e750e4c72188042aa71c6e976e360a9a3a656ae149b75a435480de23041fdf6c4364c7b52352859fa27fc3cd2c94371f16959f3343b57efa4f1ce687531845f9'
